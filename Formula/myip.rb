a89511445d3370def1b1a5eb3d306d21721d4617
Austin Traver <austintraver@users.noreply.github.com>
2020-03-12 03:25:46 -0700

    Update myip manual
    * Have command arguments represent existing commands

--- Formula/myip.rb
+++ Formula/myip.rb
@@ -2,8 +2,8 @@ class Myip < Formula
   desc "A tool to discovery your current public & private IP"
   homepage "https://github.com/austintraver/homebrew-tap/Packages/myip"
   url "https://github.com/austintraver/homebrew-tap/raw/master/Archives/myip.tgz"
-  version "2.0"
-  sha256 "3ee1a039ffc73d8b143b9f356321baaf2c6564c7ff2da6beb7f2a07189069acd"
+  version "2.1"
+  sha256 "1e5b535d926518b109ea5eb26f5deec4712f3482e65242e00041ad5a64db75c9"
 
   bottle :unneeded
   depends_on :macos => :catalina
