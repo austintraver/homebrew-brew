cbe8d63b6b79ec1a49c88c09ec9aedb1b06bf9e0
Austin Traver <austintraver@users.noreply.github.com>
2021-03-23 08:53:35 -0700

    Update formula version increment script

--- Formula/drain.rb
+++ Formula/drain.rb
@@ -2,7 +2,7 @@ class Drain < Formula
   desc "A command line tool to free up clogged ports"
   homepage "https://github.com/austintraver/homebrew-tap/Packages/drain"
   url "https://github.com/austintraver/homebrew-tap/raw/master/Archives/drain.tgz"
-  version "1.9"
+  version "1.11"
   sha256 "30fb434aee92b97d4598f1e87e43f06a4b22958024d0bf6e7d0f852434d5bfd0"
 
   bottle :unneeded
