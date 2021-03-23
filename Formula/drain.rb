674853145e50283d9659b561caeea5d621653720
Austin Traver <austintraver@users.noreply.github.com>
2021-03-23 01:36:54 -0700

    Update drain command

--- Formula/drain.rb
+++ Formula/drain.rb
@@ -2,8 +2,8 @@ class Drain < Formula
   desc "A command line tool to free up clogged ports"
   homepage "https://github.com/austintraver/homebrew-tap/Packages/drain"
   url "https://github.com/austintraver/homebrew-tap/raw/master/Archives/drain.tgz"
-  version "1.8"
-  sha256 "049b8615eaa779d999269e99e893d5d0c145bd6adc04dce2b8e88630f30ac112"
+  version "1.9"
+  sha256 "30fb434aee92b97d4598f1e87e43f06a4b22958024d0bf6e7d0f852434d5bfd0"
 
   bottle :unneeded
   depends_on "coreutils"
